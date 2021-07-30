6b5d09baadd036fda0f586e6d5d6a514ab883358
qing <cymbidium@outlook.com>
2021-07-30 13:32:35 +0800

    7.30 1.0.6

--- lib/config/base_global.dart
+++ lib/config/base_global.dart
@@ -4,7 +4,7 @@ import 'package:flutter_boss_says/config/user_controller.dart';
 import 'package:jpush_flutter/jpush_flutter.dart';
 
 class Global {
-  static String version = "1.0.3";
+  static String version = "1.0.6";
   static UserController user = UserController();
   static HintController hint = HintController();
   static EventBus eventBus = EventBus();
