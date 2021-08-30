cef377608b3508b0b78c5c3c20f8085cc6bb399c
qing <cymbidium@outlook.com>
2021-08-30 12:13:13 +0800

    8.28 1.0.13+2 需求优化 更新版本至1.0.13+1

--- lib/config/base_global.dart
+++ lib/config/base_global.dart
@@ -3,7 +3,7 @@ import 'package:flutter_boss_says/config/hint_controller.dart';
 import 'package:flutter_boss_says/config/user_controller.dart';
 
 class Global {
-  static String versionName = "1.0.14";
+  static String versionName = "1.0.13";
   static String build = "+1";
   static UserController user = UserController()..user;
   static HintController hint = HintController();
